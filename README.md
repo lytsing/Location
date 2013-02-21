572b70a19d0f3cd58f29e57ee1e021f4161c659f
Liqing Huang <hlqing@gmail.com>
2013-02-21 23:16:52 +0800

    typo and update README.md

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Put libphoneloc-jni.so, phoneloc.dat into:
 /system/usr/share/phoneloc.dat
 /system/lib/libphoneloc-jni.so
 
+### where phoneloc.dat && libphoneloc-jni.so come from?
+see https://github.com/cytown/phoneloc
+
--- src/com/android/phone/location/PhoneLocation.java
+++ src/com/android/phone/location/PhoneLocation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Lysting Huang http://lytsing.org
+ * Copyright (C) 2011 Lytsing Huang http://lytsing.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
